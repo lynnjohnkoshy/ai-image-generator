7606ddbcfe86bdb3d848074ef057d50e08c5074f
Lynn <lynnjohnkoshy@gmail.com>
2026-05-29 00:52:10 -0400

    Connected backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,47 @@
 const express = require("express");
+const cors = require("cors"); // Allows backend and frontend to communicate even if they are on different ports (no stranger danger)
 
-const app = express();
 const PORT = 8080;
+const app = express();
+
+app.use(cors())
+app.use(express.json()) // Allows to automatically read json (e.g., access like "req.body.name")
 
 const path = require("path");
-
 app.use("/", express.static(path.join(__dirname, "public")));
 console.log("__dirname:", __dirname);
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"));
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "Backend connected successfully 🚀",
+    });
 });
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/about.html"));
+app.post("/api/test", (req, res) => {
+  try {
+    console.log(req.body);
+
+    if (!req.body || !req.body.name) {
+      return res.status(400).json({
+        success: false,
+        message: "Name is required"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Data received!",
+      received: req.body
+    });
+
+  } catch (error) {
+    console.error("Server error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  async function getData() {
+    try {
+      const res = await fetch("http://localhost:8080/api/test");
+      const data = await res.json();
+      setMessage(data.message);
+    } catch (err) {
+      console.log("Error: " + err)
+    };
+  };
+
+  async function sendData() {
+    try {
+      const res = await fetch("http://localhost:8080/api/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: "Lynn"
+        })
+      });
+      const data = await res.json()
+      console.log(data);
+    } catch (err) {
+      console.log("Error: " + err)
+    }
+  };
+
   return (
     <>
-      <h1>AI Image Generator 🚀</h1>
+      <button onClick={getData}>Get Data</button>
+      <p>{message}</p>
+
+      <button onClick={sendData}>Send Data</button>
     </>
   )
 }
 
-export default App
+export default App;
